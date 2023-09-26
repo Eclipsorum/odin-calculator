3f42acaad6c7023c0da91ae3a1ee158034c1146d
Eclipsorum <eclipsorum@gmail.com>
2023-09-26 16:12:53 +0700

    Create Calculator() constructor

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function Calculator() {
+    this.calculate = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y,
+    }
+};
