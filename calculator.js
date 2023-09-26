13dbe98c8b2241c1321a005b492f44d3f5d92bbf
Eclipsorum <eclipsorum@gmail.com>
2023-09-26 20:10:02 +0700

    Add operate() to button '='

--- calculator.js
+++ calculator.js
@@ -2,7 +2,26 @@ const btn = document.querySelectorAll('button');
 const displayValue = document.querySelector('.display-value');
 
 btn.forEach(btn => btn.addEventListener("click", () => {
-    displayValue.textContent += btn.textContent;
+    if(btn.textContent == 'C') {
+        displayValue.textContent = '';
+    }
+    else if(btn.textContent == '=') {
+        const calc = new Calculator();
+        displayValue.textContent = calc.operate(displayValue.textContent);
+    }
+    else {
+        if (/[0-9]/.test(btn.textContent)) {
+            displayValue.textContent += btn.textContent;
+        }
+        else {
+            if(!(/[0-9]/.test(displayValue.textContent.slice(-1)))) {
+                displayValue.textContent = displayValue.textContent.slice(0,displayValue.textContent.length-1) + btn.textContent;
+            }
+            else {
+                displayValue.textContent += btn.textContent;
+            }
+        }
+    }
 }))
 
 function Calculator() {
@@ -13,7 +32,25 @@ function Calculator() {
         '/': (x, y) => x / y,
     };
 
-    this.operate = function(num1, operator, num2) {
-        return this.calculate[operator](num1, num2);
+    this.operate = function(str) {
+        const strSplit = str.split(/([-+/*])+/);
+        while (strSplit.some(item => /[/*]/.test(item))) {
+           const index = strSplit.findIndex(item => /[/*]/.test(item));
+           const eval = this.calculate[strSplit[index]](Number(strSplit[index-1]), Number(strSplit[index+1]));
+           strSplit.splice(index-1,3,eval);
+           console.log(strSplit);
+        }
+        while (strSplit.some(item => item == '+' || item == '-')) {
+            const index = strSplit.findIndex(item => item == '+' || item == '-');
+            console.log(strSplit[index]);
+            const eval = this.calculate[strSplit[index]](Number(strSplit[index-1]), Number(strSplit[index+1]));
+            strSplit.splice(index-1,3,eval);
+            console.log(strSplit);
+
+         }
+        return strSplit[0];
     };
 };
+//2+3/3-6*6*3+3
+2 + 1 - 36 * 3 + 3
+2+1-105
