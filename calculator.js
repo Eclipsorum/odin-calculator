263e27f48c0f5095022623398f8ae9b749c15719
Eclipsorum <eclipsorum@gmail.com>
2023-09-27 18:11:49 +0700

    Add history feature

--- calculator.js
+++ calculator.js
@@ -12,6 +12,9 @@ function keyboardInput(e) {
     if(e.key == 'Backspace') {
         inputValue('\u232B');
     }
+    if(e.key == 'c') {
+        inputValue('C');
+    }
     keys.forEach(key => {
         if(e.key == key.textContent) {
             inputValue(key.textContent);
@@ -20,34 +23,63 @@ function keyboardInput(e) {
 };
 
 function inputValue(content) {
-    if(content == 'C') {
-        displayValue.textContent = '';
-    }
-    else if(content == '.') {
-        const splitDisplay = displayValue.textContent.split(/[/*-+]/);
-        if(!(splitDisplay[splitDisplay.length-1].includes('.')) && splitDisplay[splitDisplay.length-1] != '') {
-            displayValue.textContent += content;
-        }
-    }
-    else if(content == '=') {
-        const calc = new Calculator();
-        displayValue.textContent = calc.operate(displayValue.textContent);
-    }
-    else if(content == '\u232B') {
-        displayValue.textContent = displayValue.textContent.slice(0,displayValue.textContent.length-1);
-    }
-    else {
-        if (/[0-9]/.test(content)) {
-            displayValue.textContent += content;
-        }
-        else if(!(/[0-9]/.test(displayValue.textContent.slice(-1)))) {
-            displayValue.textContent = displayValue.textContent.slice(0,displayValue.textContent.length-1) + content;
+    switch(content) {
+        case 'C':
+            displayValue.textContent = '';
+            break;
+        
+        case '\u232B':
+            displayValue.textContent = displayValue.textContent.slice(0,displayValue.textContent.length-1);
+            break;
+
+        case '=':
+            if(!(document.querySelector('.history-container'))) {
+                const mainContainer = document.querySelector('.main-container');
+                const historyDiv = document.createElement('div');
+                const historyDivH2 = document.createElement('h2');
+                const historyClear = document.createElement('button')
+                const historyItems = document.createElement('div');
+                historyDiv.setAttribute('class','history-container');
+                historyItems.setAttribute('class','history-items');
+                historyDiv.setAttribute('style',`height:${getComputedStyle(document.querySelector('.calc-container')).height}`);
+                historyDivH2.setAttribute('style', 'margin-left: 110px');
+                historyClear.setAttribute('style', 'margin-left: 100px');
+                historyClear.textContent = 'Clear History';
+                historyDivH2.textContent = 'History';
+    
+                historyClear.addEventListener('click', () => {
+                    while (historyItems.firstChild) {
+                        historyItems.removeChild(historyItems.lastChild);
+                      }
+                });
+                
+                historyDiv.appendChild(historyDivH2);
+                mainContainer.appendChild(historyDiv);   
+                historyDiv.appendChild(historyClear);  
+                historyDiv.appendChild(historyItems);        
             }
-        else {
-            displayValue.textContent += content;
-        }        
+            const calc = new Calculator();
+            const historyDiv = document.querySelector('.history-items');
+            const para = document.createElement('p');
+            para.setAttribute('style', 'margin: 10px')
+            const paraStr = displayValue.textContent;        
+            displayValue.textContent = calc.operate(displayValue.textContent);
+            para.textContent = paraStr + ' = ' + displayValue.textContent;
+            historyDiv.appendChild(para);
+            break;
+
+        default:
+            if (/[0-9]/.test(content)) {
+                displayValue.textContent += content;
+            }
+            else if(!(/[0-9]/.test(displayValue.textContent.slice(-1)))) {
+                displayValue.textContent = displayValue.textContent.slice(0,displayValue.textContent.length-1) + content;
+                }
+            else {
+                displayValue.textContent += content;
+            }     
     }
-};
+}
 
 function Calculator() {
     this.calculate = {
